16c77b0eab4e1cabb6a7ef4f8ea27df98dd788cc
Levaillant Clement <clemlvt42@gmail.com>
2019-03-29 18:40:06 +0100

    ok

--- camera.php
+++ camera.php
@@ -1,104 +1,50 @@
 <?php require_once "inc/bootstrap.php";
 $auth = App::getAuth()->restrict();
-
 ?>
 <?php require 'inc/header.php'; ?>
 
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <style type="text/css">
-    html {
-      height: 100%;
-    }
-
-    h1 {
-      text-align: center;
-    }
-
-    video, canvas, #startbutton, #send_snap {
-      display: block;
-      float: left;
-      border: 2.5px solid grey;
-      border-radius: 10px;
-    }
-    #startbutton {
-      background: green;
-      border: none;
-      color: #fff;
-      margin: 100px 20px 20px 20px;
-      padding: 10px 20px;
-      font-size: 20px;
-    }
-    #container {
-      overflow: hidden;
-      width: 880px;
-      margin: 20px auto;
-    }
-
-    #send_snap {
-      background: blue;
-      border: none;
-      color: #fff;
-      margin: 100px 20px 20px 20px;
-      padding: 10px 20px;
-      font-size: 20px;
-      margin : auto;
-    }
-  </style>
-</head>
-
-<body>
-
-  <h1>Caméra</h1>
-  <div id="container">
-    <video id="video"></video>
-    <button id="startbutton">Prendre la photo</button>
-    <canvas id="canvas"></canvas>
-    <button id="send_snap">Envoyer</button>
-  </div>
-
+<div class="camera">
+  <video id="video">Video stream not available</video>
+  <button id="startbutton">Take photo</button>
+</div>
+<canvas id="canvas">
+</canvas>
+<div class="output">
+  <img id="photo" alt="The screen capture will appear in this box.">
+</div>
 <script type="text/javascript">
 
 (function() {
+  var width = 320;
+  var height = 0;
 
-  var streaming = false,
-      video        = document.querySelector('#video'),
-      cover        = document.querySelector('#cover'),
-      canvas       = document.querySelector('#canvas'),
-      photo        = document.querySelector('#photo'),
-      startbutton  = document.querySelector('#startbutton'),
-      width = 320,
-      height = 0;
+  var streaming = false;
 
-  navigator.getMedia = ( navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia);
+  var video = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
+})
 
-  navigator.getMedia(
-    {
-      video: true,
-      audio: false
-    },
-    function(stream) {
-      if (navigator.mozGetUserMedia) {
-        video.mozSrcObject = stream;
-      } else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.srcObject = stream;
-      }
+function startup() {
+  video = document.getElementById('video');
+  canvas = document.getElementById('canvas');
+  photo = document.getElementById('photo');
+  startbutton = document.getElementsById('startbutton');
+
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function(stream) {
+      video.srcObject = stream;
       video.play();
-    },
-    function(err) {
-      console.log("An error occured! " + err);
-    }
-  );
+    })
+    .catch(function(err) {
+      console.log("An error occured: " + err);
+    });
 
-  video.addEventListener('canplay', function(ev){
+  video.addEventListener('canplay', function(ev) {
     if (!streaming) {
-      height = video.videoHeight / (video.videoWidth/width);
+      height = video.videoHeight / (video.videoWidth);
+
       video.setAttribute('width', width);
       video.setAttribute('height', height);
       canvas.setAttribute('width', width);
@@ -107,32 +53,34 @@ $auth = App::getAuth()->restrict();
     }
   }, false);
 
-  function takepicture() {
-    canvas.width = width;
-    canvas.height = height;
-    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
-    var data = canvas.toDataURL('image/jpeg');
-    photo.setAttribute('src', data);
-  }
-
-  function uploadimage() {
-    
-  }
-
-  startbutton.addEventListener('click', function(ev){
-      takepicture();
+  startbutton.addEventListener('click', function(ev) {
+    takepicture();
     ev.preventDefault();
   }, false);
+  clearphoto();
+}
 
-  startbutton.addEventListener('click', function(ev)){
+function clearphoto() {
+  var context = canvas.getContext('2d');
+  context.fillStyle = "#AAA";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
+  var data = canvas.toDataURL('image/png');
+  photo.setAttribute('src', data);
+}
+
+function takePicture() {
+  var context = canvas.getContext('2d');
+  if (width && height) {
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
+
+    var data = canvas/toDataURL('image/png');
+    photo.setAttribute('src', data);
+  } else {
+    clearphoto();
   }
-
-})();
+}
 
 </script>
-</body>
-</html>
-
-<?php require 'inc/footer.php'; ?>
-
